abfe5296df268cec57de495dddf6a15e1ec23be3
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-11-26 19:34:20 +0700

    feat: Import Game Icons

---
+++ src/assets/game-icons.ts
@@ -0,0 +1,23 @@
+import gameIcon1 from '../../public/images/game_icons/game_11.svg'
+import gameIcon2 from '../../public/images/game_icons/game_12.svg'
+import gameIcon3 from '../../public/images/game_icons/game_13.svg'
+import gameIcon4 from '../../public/images/game_icons/game_14.svg'
+import gameIcon5 from '../../public/images/game_icons/game_15.svg'
+import gameIcon6 from '../../public/images/game_icons/game_16.svg'
+import gameIcon7 from '../../public/images/game_icons/game_17.svg'
+import gameIcon8 from '../../public/images/game_icons/game_18.svg'
+import gameIcon9 from '../../public/images/game_icons/game_19.svg'
+import gameIcon10 from '../../public/images/game_icons/game_20.svg'
+
+export {
+  gameIcon1,
+  gameIcon2,
+  gameIcon3,
+  gameIcon4,
+  gameIcon5,
+  gameIcon6,
+  gameIcon7,
+  gameIcon8,
+  gameIcon9,
+  gameIcon10
+}
--- src/components/home-page/sections/games-available/games-available-section.tsx
+++ src/components/home-page/sections/games-available/games-available-section.tsx
@@ -1,3 +1,4 @@
+import { gameIcon1, gameIcon2, gameIcon3, gameIcon4, gameIcon5, gameIcon6 } from '@/assets/game-icons'
 import Button from '@/components/common/button'
 import { illustKoin } from '@/components/common/illustration'
 import Image from 'next/image'
@@ -13,34 +14,54 @@ const GamesAvailableSection = () => {
   const games: Game[] = [
     {
       name: 'Game 1',
-      image: illustKoin,
+      image: gameIcon1,
       url: 'https://game1.com'
     },
     {
       name: 'Game 2',
-      image: illustKoin,
+      image: gameIcon2,
       url: 'https://game2.com'
     },
     {
       name: 'Game 3',
-      image: illustKoin,
+      image: gameIcon3,
       url: 'https://game3.com'
     },
     {
       name: 'Game 4',
-      image: illustKoin,
+      image: gameIcon4,
       url: 'https://game4.com'
     },
     {
       name: 'Game 5',
-      image: illustKoin,
+      image: gameIcon5,
       url: 'https://game5.com'
     },
     {
       name: 'Game 6',
-      image: illustKoin,
+      image: gameIcon6,
       url: 'https://game6.com'
     },
+    {
+      name: 'Game 7',
+      image: gameIcon1,
+      url: 'https://game7.com'
+    },
+    {
+      name: 'Game 8',
+      image: gameIcon2,
+      url: 'https://game8.com'
+    },
+    {
+      name: 'Game 9',
+      image: gameIcon3,
+      url: 'https://game9.com'
+    },
+    {
+      name: 'Game 10',
+      image: gameIcon4,
+      url: 'https://game10.com'
+    }
   ]
 
   return (
@@ -48,9 +69,9 @@ const GamesAvailableSection = () => {
       <div className='pt-[100px] pb-[80px] mx-auto flex flex-col items-center gap-[60px] md:gap-[100px]'>
         <h2 className='h2 text-center'>Games <span>Available</span></h2>
 
-        <div className='games-row flex flex-row gap-[40px] min-w-full w-fit bg-red-50 items-center justify-center'>
+        <div className='games-row flex flex-row gap-[40px] min-w-full w-fit items-center justify-center'>
           {games.map((game, index) => (
-            <div key={index} className='game-card bg-gray-200 rounded-lg shadow-md h-[120px] w-[120px] '>
+            <div key={index} className='game-card rounded-[20px] shadow-xl h-[120px] w-[120px]  overflow-clip'>
               <Image src={game.image} alt={game.name} className='w-full h-full object-cover' />
             </div>
           ))}
