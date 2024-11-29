cbe0aac760838812369e8f764a7d7e3ceb4597ba
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-11-29 19:49:54 +0700

    feat: Add Static URL Files

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/documents/:file',
+        destination: '/documents/:file',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
